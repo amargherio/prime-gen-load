4282fc8b3edd110ca97c3fd3a75fd54f11684a7c
Adam Margherio <margherio.adam@gmail.com>
2021-11-09 23:24:30 -0600

    initial pass on pod creation flow

--- pod-generator/src/main.rs
+++ pod-generator/src/main.rs
@@ -1,35 +1,225 @@
-use actix_web::{
-    error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
-};
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, error, http::StatusCode, middleware, web};
 use futures::StreamExt;
 use json::JsonValue;
+use k8s_openapi::api::{apps::v1::Deployment, core::v1::{Namespace, Pod, Service}};
+use kube::{Api, Client, api::PostParams};
+use rand::{Rng, distributions::Alphanumeric, thread_rng};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tracing::{span, Level};
 use tracing_actix_web::TracingLogger;
 
+#[derive(Debug, Deserialize, Serialize)]
 struct WorkloadConfig {
     count: usize,
 }
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     // init tracing logging
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
     App::new()
         // logging
-        .wrap(TracingLogger)
-        .service(
-            web::resource("/init").route(web::put().to(init_workload)))
+        .wrap(TracingLogger::default())
+        .route("/init", web::put().to(init_workload))
     })
     .bind("127.0.0.1:8080")?
     .run()
-    .await
+    .await?;
+
+    Ok(())
+
 }
 
 #[tracing::instrument]
-async fn init_workload(web::Query(count): web::Query<WorkloadConfig>) -> HttpResponse{
-    // send the WorkloadConfig over a channel for async work and send the accepted response?
+async fn init_workload(workload: web::Query<WorkloadConfig>) -> HttpResponse {
+    // create the initial Kube API client and create the target namespace for the workload (randomly-generated name used)
+    let client = Client::try_default().await.unwrap();
+    let target_ns = gen_target_ns().await.unwrap();
+    tracing::debug!("Generated namespace value {}", target_ns);
+    let ns_api: Api<Namespace> = Api::all(client.clone());
 
-    HttpResponse::Accepted()
+    let ns: Namespace = serde_json::from_value(json!({
+        "apiVersion": "v1",
+        "kind": "Namespace",
+        "metadata": {
+            "name": target_ns
+        }
+    })).unwrap();
+    
+    let ns_params = PostParams::default();
+    match ns_api.create(&ns_params, &ns).await {
+        Ok(n) => {
+            let name = n.metadata.name.unwrap();
+            tracing::info!("Created namespace {}", name);
+        },
+        Err(kube::Error::Api(ae)) => {
+            // handle kubernetes specific errors here. this will most likely result in death
+            // but needs more specific handling
+            if ae.code == 401 {
+                tracing::error!("Received an unauthorized response from the API server when creating namespace {}", target_ns);
+            } else if ae.code == 429 {
+                tracing::warn!("Received throttled response from API server - message: {}", ae.message);
+            } else {
+                tracing::warn!("Error occurred while attempting to interact with the API server. Status: {}, message: {}", ae.status, ae.message);
+            }
+            return HttpResponse::InternalServerError().finish();
+        },
+        Err(e) => {
+            unimplemented!()
+        }
+    }
+
+    if workload.count <= 0 {
+        tracing::warn!("Received request to spin up zero or a negative pod count - returning.");
+        return HttpResponse::BadRequest().finish();
+    }
+    tracing::debug!("Spinning up {} pods to calculate primes via sieve.", workload.count);
+
+    deploy_instance_service(client.clone(), &target_ns).await;
+
+    let pod_api: Api<Pod> = Api::namespaced(client.clone(), &target_ns);
+    
+    for n in 0..=workload.count {
+        let pod_def: Pod = serde_json::from_value(json!({
+            "apiVersion": "v1",
+            "kind": "Pod",
+            "metadata": {
+                "name": format!("prime-sieve-instance-{}", n),
+                "namespace": target_ns,
+            },
+            "spec": {
+                "containers": [
+                    {
+                        "image": "amartest.azurecr.io/apps/slb/prime-generator:0.1.0",
+                        "name": "prime-generator"
+                    }
+                ],
+
+            }
+        })).unwrap();
+
+        match pod_api.create(&PostParams::default(), &pod_def).await {
+            Ok(p) => {
+                tracing::debug!("Created new pod {} in namespace {}", format!("prime-sieve-instance-{}", n), target_ns);
+            },
+            Err(kube::Error::Api(ae)) => {
+                // handle kubernetes specific errors here. this will most likely result in death
+                // but needs more specific handling
+                if ae.code == 401 {
+                    tracing::error!("Received an unauthorized response from the API server when creating namespace {}", target_ns);
+                } else if ae.code == 429 {
+                    tracing::warn!("Received throttled response from API server - message: {}", ae.message);
+                } else {
+                    tracing::warn!("Error occurred while attempting to interact with the API server. Status: {}, message: {}", ae.status, ae.message);
+                }
+                return HttpResponse::InternalServerError().finish();
+            },
+            Err(e) => {
+                unimplemented!()
+            }
+        }
+    }
+
+    HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(skip(client))]
+async fn deploy_instance_service(client: Client, target_ns: &str) {
+    // create instance service deployment and headless service in cluster
+    let deploy_api: Api<Deployment> = Api::namespaced(client.clone(), target_ns);
+    let service_api: Api<Service> = Api::namespaced(client.clone(), target_ns);
+
+    let dep: Deployment = serde_json::from_value(json!({
+        "apiVersion": "apps/v1",
+        "kind": "Deployment",
+        "metadata": {
+            "name": "instance-service",
+            "namespace": target_ns,
+        },
+        "spec": {
+            "replicas": 1,
+            "selector": {
+                "matchLabels": {
+                    "app": "instance-service"
+                }
+            },
+            "template": {
+                "metadata": {
+                    "labels": {
+                        "app": "instance-service"
+                    }
+                },
+                "spec": {
+                    "containers": [
+                        {
+                            "name": "instance-service",
+                            "image": "amartest.azurecr.io/apps/slb/instance-service:0.1.0",
+                            "ports": [
+                                {
+                                    "containerPort": 8080
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    })).unwrap();
+
+    let headless: Service = serde_json::from_value(json!({
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {
+            "name": "instance-service-headless"
+        },
+        "spec": {
+            "clusterIP": "None",
+            "selector": {
+                "matchLabels": {
+                    "app": "instance-service"
+                }
+            },
+            "ports": [
+                {
+                    "protocol": "TCP",
+                    "port": 8080,
+                    "targetPort": 8080
+                }
+            ]
+        }
+    })).unwrap();
+    let params = PostParams::default();
+
+    match deploy_api.create(&PostParams::default(), &dep).await {
+        Ok(_) => {
+            tracing::debug!("Created instance service deployment in target namesace '{}'", target_ns);
+        },
+        Err(_) => {
+            todo!()
+        }
+    }
+
+    match service_api.create(&PostParams::default(), &headless).await {
+        Ok(_) => {
+            tracing::debug!("Created headless service in target namespace '{}'", target_ns)
+        },
+        Err(_) => {
+            todo!()
+        }
+    }
+}
+
+#[tracing::instrument]
+async fn gen_target_ns() -> anyhow::Result<String> {
+    let mut rng = thread_rng();
+
+    let ns = (&mut rng)
+    .sample_iter(Alphanumeric)
+    .take(8)
+    .map(char::from)
+    .collect::<String>();
+
+    Ok(ns)
 }
