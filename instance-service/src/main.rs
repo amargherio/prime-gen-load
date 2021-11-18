76c2d9674c779bb492cfce5cc30c2f0076d8577f
Adam Margherio <margherio.adam@gmail.com>
2021-11-18 00:03:32 -0600

    cleaned up imports and minor log adjustments

--- instance-service/src/main.rs
+++ instance-service/src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}, thread::sleep, time::Duration};
+use std::{collections::HashMap, sync::Mutex, thread::sleep, time::Duration};
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tracing_actix_web::TracingLogger;
@@ -32,10 +32,9 @@ struct AppData {
 async fn main() -> anyhow::Result<()> {
     // init tracing logging
     tracing_subscriber::fmt::init();
-    tracing::info!("Introducing a slight delay to represent establishing database connections and other operations.");
-    let dur = rand::thread_rng().gen_range(1750..=5250);
+    tracing::info!("Mimicking database connection pool initialization and connections to messaging clients");
+    let dur = rand::thread_rng().gen_range(3000..=5250);
     sleep(Duration::from_millis(dur));
-
 
     // init datastore for instance service
     let hmap: HashMap<String, Worker> = HashMap::new();
@@ -43,6 +42,7 @@ async fn main() -> anyhow::Result<()> {
     let store = web::Data::new(Mutex::new(AppData {
         sieve_map: hmap,
     }));
+    tracing::info!("Completed hashmap creation for storing results.");
 
     HttpServer::new(move || {
     App::new()
@@ -60,7 +60,7 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[tracing::instrument(skip(sieve))]
+#[tracing::instrument(skip(store))]
 async fn register_sieve(store: web::Data<Mutex<AppData>>, sieve: web::Json<Sieve>) -> HttpResponse {
     let worker = Worker { id: sieve.id.clone(), result: None };
     let id = sieve.id.clone();
@@ -68,7 +68,7 @@ async fn register_sieve(store: web::Data<Mutex<AppData>>, sieve: web::Json<Sieve
     let mut hstore = store.try_lock().unwrap();
     let mut hmap = &mut hstore.sieve_map;
         
-    tracing::debug!("Inserting ID '{}' and worker {:?} into hstore", id, worker);
+    tracing::info!("Inserting ID '{}' and worker {:?} into hstore", id, worker);
     hmap.insert(id, worker);
     let dur = rand::thread_rng().gen_range(400..=1000);
     sleep(Duration::from_millis(dur));
@@ -76,7 +76,7 @@ async fn register_sieve(store: web::Data<Mutex<AppData>>, sieve: web::Json<Sieve
     HttpResponse::Created().finish()
 }
 
-#[tracing::instrument(skip(payload))]
+#[tracing::instrument(skip(payload, store))]
 async fn save_result(store: web::Data<Mutex<AppData>>, payload: web::Json<SieveResult>) -> HttpResponse {
     let mut hstore = store.try_lock().unwrap();
     let mut hmap = &mut hstore.sieve_map;
@@ -111,5 +111,6 @@ async fn save_result(store: web::Data<Mutex<AppData>>, payload: web::Json<SieveR
 
 #[tracing::instrument]
 async fn health_check() -> HttpResponse {
+    tracing::info!("Responding to health check request with OK response.");
     HttpResponse::Ok().finish()
 }
